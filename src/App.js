633bfe772f7141a998e10dfad10d42bdd101c608
atp <atp.aung.5555@gmail.com>
2023-04-08 22:56:02 +0630

    d

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ const Statistics = (p) => {
   return (
     <>
       <h1>Statistics</h1>
-      <p>good {p.good}</p>
-      <p>neutral {p.neutral}</p>
-      <p>bad {p.bad}</p>
-      <p>all {p.all}</p>
-      <p>average {p.avg}</p>
-      <p>postive {p.pos * 100} %</p>
+      <p>good: {p.good}</p>
+      <p>neutral: {p.neutral}</p>
+      <p>bad: {p.bad}</p>
+      <p>all: {p.all}</p>
+      <p>average: {p.avg}</p>
+      <p>postive: {p.pos * 100} %</p>
     </>
   );
 };
